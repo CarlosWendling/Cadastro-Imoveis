fde570a5d7ce730d9aa06da04034d281021254c7
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-09-26 11:28:29 -0300

    create user register interface

--- cadastro-user.php
+++ cadastro-user.php
@@ -1,47 +1,9 @@
-<?php
-include('conexao.php');
-
-// Verifivando a existência do email e da senha
-if (isset($_POST['email']) && isset($_POST['senha'])) {
-
-    if (strlen($_POST['email']) == 0) {
-        echo '<p class="alert alert-danger m-4 w-25 position-absolute top-0" style="top: 20%; left: 50%; transform: translate(-50%, 0); z-index: 5;">Preencha o campo de email.</p>';
-    } else if (strlen($_POST['senha']) == 0) {
-        echo '<p class="alert alert-danger m-4 w-25 position-absolute top-0" style="top: 20%; left: 50%; transform: translate(-50%, 0); z-index: 5;">Preencha com a sua senha.</p>';
-    } else {
-        // Limpar a string da variável
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $senha = $mysqli->real_escape_string($_POST['senha']);
-
-        $sql_code = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
-        $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: ".$mysqli->error);
-
-        $quantidade = $sql_query->num_rows;
-
-        if ($quantidade == 1) {
-            $usuario = $sql_query->fetch_assoc();
-
-            if (!isset($_SESSION)) {
-                session_start();
-            }
-
-            $_SESSION['id'] = $usuario['id'];
-            $_SESSION['nome'] = $usuario['nome'];
-
-            header("Location: menu.php");
-        } else {
-            echo 'Falha ao logar. Email ou senha incorretos';
-        }
-    }
-}
-?>
-
 <!doctype html>
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login</title>
+    <title>Cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="src/style.css">
   </head>
@@ -66,22 +28,30 @@ if (isset($_POST['email']) && isset($_POST['senha'])) {
     </nav>
 
     <main>
-        <h1>Login</h1>
+        <h1 style="margin-top: 4rem;">Cadastro</h1>
         
         <form class="user-form" method="post">
             <div class="mb-3">
-                <label for="login-email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="login-email" name="email" require>
+                <label for="cadastro-nome" class="form-label">Nome Completo</label>
+                <input type="text" class="form-control" id="cadastro-nome" name="nome" require>
             </div>
             <div class="mb-3">
-                <label for="login-pass" class="form-label">Senha</label>
-                <input type="password" class="form-control" id="login-pass" name="senha" require>
+                <label for="cadastro-email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="cadastro-email" name="email" require>
+            </div>
+            <div class="mb-3">
+                <label for="cadastro-pass" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="cadastro-pass" name="senha" require>
+            </div>
+            <div class="mb-3">
+                <label for="confirm-pass" class="form-label">Confirme a Senha</label>
+                <input type="password" class="form-control" id="confirm-pass" name="confirm-senha" require>
             </div>
             <div class="mb-3">
                 <button type="submit" class="btn btn-primary form-control" value="entrar" style="margin-top: 0.4rem;">Entrar</button>
             </div>
             <div class="mb-3">
-                <p>Ainda não possui Cadastro? <a href="./cadastro-user.php">Clique aqui</a></p>
+                <p>Já possui Cadastro? <a href="./index.php">Clique aqui</a></p>
             </div>
         </form>
     </main>
