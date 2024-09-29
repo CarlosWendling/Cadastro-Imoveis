8befb413232a7090a28a197f13216d353c37e200
Carlos Eduardo Polidori Wendling <cadupolidori@gmail.com>
2024-09-29 12:23:34 -0300

    create edit system

--- acoes.php
+++ acoes.php
@@ -17,4 +17,24 @@ if (isset($_POST['adicionar_proprietario'])) {
     header("Location: gerenciar-proprietario.php");
     
 }
+
+if (isset($_POST['update_proprietario'])) {
+    $usuario_id = mysqli_real_escape_string($mysqli, $_POST['usuario_id']);
+
+    $nome = mysqli_real_escape_string($mysqli, trim($_POST['nome']));
+    $data_nascimento = mysqli_real_escape_string($mysqli, trim($_POST['data_nascimento']));
+    $cpf = mysqli_real_escape_string($mysqli, $_POST['cpf']);
+    $sexo = mysqli_real_escape_string($mysqli, trim($_POST['sexo']));
+    $telefone = mysqli_real_escape_string($mysqli, $_POST['telefone']);
+    $email = mysqli_real_escape_string($mysqli, trim($_POST['email']));
+
+    $sql_code = "UPDATE proprietarios SET nome = '$nome', data_nascimento = '$data_nascimento', cpf = '$cpf', sexo = '$sexo', telefone = '$telefone', email = '$email'";
+
+    $sql_code .= "WHERE id = '$usuario_id'";
+    
+    mysqli_query($mysqli, $sql_code);
+    
+    header("Location: gerenciar-proprietario.php");
+    
+}
 ?>
---
+++ proprietario-edit.php
@@ -0,0 +1,104 @@
+<?php
+include('protect.php');
+include('conexao.php');
+?>
+
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Proprietários</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/style.css">
+  </head>
+  <body>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="box-shadow: 1px 1px 5px rgb(0, 0, 0, 0.3);">
+        <div class="container-fluid d-flex p-2"  style="margin: 0 3rem;">
+            <a class="navbar-brand" href="#"><img class="img-fluid" src="./src/img/logo-prefeitura.png"></a>
+
+            <form class="d-flex" role="search" style="width: 40%" action="" method="POST">
+                <input class="form-control me-2" type="search" placeholder="O que você procura" aria-label="Search" style="height: 43px; font-size: 0.8rem;">
+                <button class="btn btn-primary form-control" style="width: 4rem; color: #fff; font-size: 0.8rem;" type="submit">Buscar</button>
+            </form>
+
+            <section class="links">
+                <a href="#" target="_blank"><img class="img-fluid" src="./src/img/logo-facebook.png" style="width: 30px;"></a>
+                <a href="https://www.instagram.com/prefasaoleo/" target="_blank">
+                    <img class="img-fluid" src="./src/img/logo-instagram.png" style="width: 25px; margin: 0 5px;"></a>
+                <a href="#" target="_blank"><img class="img-fluid" src="./src/img/logo-youtube.png" style="width: 30px"></a>
+            </section>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>
+                            Editar Proprietário
+                            <a href="gerenciar-proprietario.php" class="btn btn-danger float-end">Voltar</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                    <?php
+                        if (isset($_GET['id'])) {
+                            $proprietario_id = mysqli_real_escape_string($mysqli, $_GET['id']);
+                            $sql_code = "SELECT * FROM proprietarios WHERE id = '$proprietario_id'";
+                            $query = mysqli_query($mysqli, $sql_code);
+
+                            if (mysqli_num_rows($query) > 0) {
+                                $proprietario = mysqli_fetch_array($query);
+                            }
+                        ?>
+
+                        <form action="acoes.php" method="post">
+                            <input type="hidden" name="usuario_id" value="<?=$proprietario['id']?>">
+                            <div class="mb-3">
+                                <label>Nome</label>
+                                <input type="text" name="nome" value="<?=$proprietario['nome']?>" class="form-control" required>
+                            </div>
+                            <div class="mb-3">
+                                <label>Data de Nascimento</label>
+                                <input type="date" name="data_nascimento" value="<?=$proprietario['data_nascimento']?>" class="form-control" required>
+                            </div>
+                            <div class="mb-3">
+                                <label>CPF</label>
+                                <input type="text" name="cpf" class="form-control" value="<?=$proprietario['cpf']?>" placeholder="000.000.000-00" autocomplete="off" minlength="11" maxlength="11" required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="sexo">Sexo</label>
+                                <select class="form-select" name="sexo" required>
+                                    <option value="<?=$proprietario['sexo']?>">Atual: <?=$proprietario['sexo']?></option>
+                                    <option value="Masculino">Masculino</option>
+                                    <option value="Feminino">Feminino</option>
+                                    <option value="Outro">Outro</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label>Telefone</label>
+                                <input type="text" name="telefone" class="form-control" value="<?=$proprietario['telefone']?>" placeholder="(00) 00000-0000" minlength="11" maxlength="11">
+                            </div>
+                            <div class="mb-3">
+                                <label>Email</label>
+                                <input type="text" name="email" value="<?=$proprietario['email']?>" class="form-control">
+                            </div>
+                            <button type="submit" name="update_proprietario" class="btn btn-primary float-middle">Atualizar</button>
+                        </form>
+                        <?php
+                        } else {
+                            echo '<h5>Proprietário não encontrado</h5>';
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+  </body>
+</html>
